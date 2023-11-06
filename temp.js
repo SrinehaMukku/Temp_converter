cc4c174609a7368368905ec54389b1be510f1fdb
Srineha_mukku <143319554+SrinehaMukku@users.noreply.github.com>
2023-11-06 23:54:56 +0530

    js updated

---
+++ temp.js
@@ -0,0 +1,5 @@
+function convertToFar() {
+    var celsius = parseFloat(document.getElementById("text").value);
+    var fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById("result").innerHTML = fahrenheit + " °F";
+}
